f893da473830a5c9f2603ce9076bdf763e80946c
Jared Sutch <sutch002@gmail.com>
2021-05-27 20:43:54 -0500

    updated script

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,10 +16,10 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag, as: "product_tags" });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag, as: "tagged_products" });
 
 module.exports = {
   Product,
